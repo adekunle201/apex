bcdb1aede4cf58bbac4cc512af238b666d3569fd
Kunle Adesina <kunleadesina201@gmail.com>
2025-12-02 15:47:13 +0100

    Exception to catch error import_app_102.sql

--- scripts/import_app_102.sql
+++ scripts/import_app_102.sql
@@ -1,6 +1,8 @@
 -- =====================================================
 -- 1. Establish workspace context for APEX import
 -- =====================================================
+
+
 BEGIN
     apex_application_install.set_workspace('UAT');
     apex_application_install.set_workspace_id(
@@ -15,6 +17,27 @@ END;
 -- =====================================================
 -- 2. Run the APEX application installer
 -- =====================================================
-@apex/app/app_102/f102/install.sql
+DECLARE
+    v_errm   VARCHAR2(4000);
+    v_stack  VARCHAR2(4000);
+begin
+    @apex/app/app_102/f102/install.sql
+    EXCEPTION
+        WHEN OTHERS THEN
+            v_errm  := SQLERRM;
+            v_stack := DBMS_UTILITY.FORMAT_ERROR_BACKTRACE;
+
+            DBMS_OUTPUT.PUT_LINE('===============================');
+            DBMS_OUTPUT.PUT_LINE(' APEX IMPORT ERROR OCCURRED');
+            DBMS_OUTPUT.PUT_LINE('-------------------------------');
+            DBMS_OUTPUT.PUT_LINE('Error message: ' || v_errm);
+            DBMS_OUTPUT.PUT_LINE('Error stack:');
+            DBMS_OUTPUT.PUT_LINE(v_stack);
+            DBMS_OUTPUT.PUT_LINE('===============================');
+
+            -- RAISE AGAIN so CI/CD fails normally
+            RAISE;
+    END;
+
 
 exit;
