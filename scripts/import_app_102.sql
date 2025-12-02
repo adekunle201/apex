4cfe2b83cfc1687433908e73927bc070cc986904
Kunle Adesina <kunleadesina201@gmail.com>
2025-12-02 16:04:07 +0100

    Update import_app_102.sql

--- scripts/import_app_102.sql
+++ scripts/import_app_102.sql
@@ -1,7 +1,9 @@
 -- =====================================================
 -- 1. Establish workspace context for APEX import
 -- =====================================================
-
+DECLARE
+    v_errm   VARCHAR2(4000);
+    v_stack  VARCHAR2(4000);
 
 BEGIN
     apex_application_install.set_workspace('UAT');
@@ -11,6 +13,26 @@ BEGIN
     apex_application_install.set_application_id(null);
     --apex_application_install.set_application_alias('APP_102');
     apex_application_install.generate_offset;
+
+@apex/app/app_102/f102/install.sql
+
+    EXCEPTION
+        WHEN OTHERS THEN
+            v_errm  := SQLERRM;
+            v_stack := DBMS_UTILITY.FORMAT_ERROR_BACKTRACE;
+
+            DBMS_OUTPUT.PUT_LINE('===============================');
+            DBMS_OUTPUT.PUT_LINE(' APEX IMPORT ERROR OCCURRED');
+            DBMS_OUTPUT.PUT_LINE('-------------------------------');
+            DBMS_OUTPUT.PUT_LINE('Error message: ' || v_errm);
+            DBMS_OUTPUT.PUT_LINE('Error stack:');
+            DBMS_OUTPUT.PUT_LINE(v_stack);
+            DBMS_OUTPUT.PUT_LINE('===============================');
+
+            -- RAISE AGAIN so CI/CD fails normally
+            RAISE;
+
+
 END;
 
 exit;
