897416f7dcf4245691fb2f7c4cb3f29a2da06d7e
Kunle Adesina <kunleadesina201@gmail.com>
2025-12-02 18:11:49 +0100

    Update import_app_102.sql

--- scripts/import_app_102.sql
+++ scripts/import_app_102.sql
@@ -7,56 +7,56 @@ begin
 end;
 /
 
-@@application/set_environment.sql
-@@application/delete_application.sql
-@@application/create_application.sql
+@@../apex/app/app_102/f102/application/set_environment.sql
+@@../apex/app/app_102/f102/application/delete_application.sql
+@@../apex/app/app_102/f102/application/create_application.sql
 
-@@application/application_properties.sql
-@@application/user_interfaces.sql
+@@../apex/app/app_102/f102/application/application_properties.sql
+@@../apex/app/app_102/f102/application/user_interfaces.sql
 
-@@application/shared_components/navigation/lists/navigation_menu.sql
-@@application/shared_components/navigation/lists/navigation_bar.sql
-@@application/shared_components/navigation/listentry.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/lists/navigation_menu.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/lists/navigation_bar.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/listentry.sql
 
-@@application/shared_components/files/icons_app_icon_512_png.sql
-@@application/shared_components/files/icons_app_icon_32_png.sql
-@@application/shared_components/files/icons_app_icon_144_rounded_png.sql
-@@application/shared_components/files/icons_app_icon_192_png.sql
-@@application/shared_components/files/icons_app_icon_256_rounded_png.sql
+@@../apex/app/app_102/f102/application/shared_components/files/icons_app_icon_512_png.sql
+@@../apex/app/app_102/f102/application/shared_components/files/icons_app_icon_32_png.sql
+@@../apex/app/app_102/f102/application/shared_components/files/icons_app_icon_144_rounded_png.sql
+@@../apex/app/app_102/f102/application/shared_components/files/icons_app_icon_192_png.sql
+@@../apex/app/app_102/f102/application/shared_components/files/icons_app_icon_256_rounded_png.sql
 
-@@application/plugin_settings.sql
-@@application/shared_components/security/authorizations/administration_rights.sql
-@@application/shared_components/navigation/navigation_bar.sql
-@@application/shared_components/logic/application_settings.sql
+@@../apex/app/app_102/f102/application/plugin_settings.sql
+@@../apex/app/app_102/f102/application/shared_components/security/authorizations/administration_rights.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/navigation_bar.sql
+@@../apex/app/app_102/f102/application/shared_components/logic/application_settings.sql
 
-@@application/shared_components/navigation/tabs/standard.sql
-@@application/shared_components/navigation/tabs/parent.sql
-@@application/pages/page_groups.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/tabs/standard.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/tabs/parent.sql
+@@../apex/app/app_102/f102/application/pages/page_groups.sql
 
-@@application/shared_components/navigation/breadcrumbs/breadcrumb.sql
-@@application/shared_components/navigation/breadcrumbentry.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/breadcrumbs/breadcrumb.sql
+@@../apex/app/app_102/f102/application/shared_components/navigation/breadcrumbentry.sql
 
-@@application/shared_components/user_interface/templates/popuplov.sql
-@@application/shared_components/user_interface/themes.sql
-@@application/shared_components/user_interface/theme_style.sql
-@@application/shared_components/user_interface/theme_files.sql
-@@application/shared_components/user_interface/template_opt_groups.sql
-@@application/shared_components/user_interface/template_options.sql
+@@../apex/app/app_102/f102/application/shared_components/user_interface/templates/popuplov.sql
+@@../apex/app/app_102/f102/application/shared_components/user_interface/themes.sql
+@@../apex/app/app_102/f102/application/shared_components/user_interface/theme_style.sql
+@@../apex/app/app_102/f102/application/shared_components/user_interface/theme_files.sql
+@@../apex/app/app_102/f102/application/shared_components/user_interface/template_opt_groups.sql
+@@../apex/app/app_102/f102/application/shared_components/user_interface/template_options.sql
 
-@@application/shared_components/globalization/language.sql
-@@application/shared_components/logic/build_options.sql
-@@application/shared_components/globalization/messages.sql
-@@application/shared_components/globalization/dyntranslations.sql
+@@../apex/app/app_102/f102/application/shared_components/globalization/language.sql
+@@../apex/app/app_102/f102/application/shared_components/logic/build_options.sql
+@@../apex/app/app_102/f102/application/shared_components/globalization/messages.sql
+@@../apex/app/app_102/f102/application/shared_components/globalization/dyntranslations.sql
 
-@@application/shared_components/security/authentications/oracle_apex_accounts.sql
+@@../apex/app/app_102/f102/application/shared_components/security/authentications/oracle_apex_accounts.sql
 
-@@application/user_interfaces/combined_files.sql
+@@../apex/app/app_102/f102/application/user_interfaces/combined_files.sql
 
-@@application/pages/page_00000.sql
-@@application/pages/page_00001.sql
-@@application/pages/page_09999.sql
+@@../apex/app/app_102/f102/application/pages/page_00000.sql
+@@../apex/app/app_102/f102/application/pages/page_00001.sql
+@@../apex/app/app_102/f102/application/pages/page_09999.sql
 
-@@application/deployment/definition.sql
-@@application/deployment/checks.sql
-@@application/deployment/buildoptions.sql
-@@application/end_environment.sql
+@@../apex/app/app_102/f102/application/deployment/definition.sql
+@@../apex/app/app_102/f102/application/deployment/checks.sql
+@@../apex/app/app_102/f102/application/deployment/buildoptions.sql
+@@../apex/app/app_102/f102/application/end_environment.sql
